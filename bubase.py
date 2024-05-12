1e5687ef979ba52ed9ba49ae416cf7a9f698c8c0
mz <0922366@zju.edu.cn>
2024-05-12 12:37:34 +0800

    reveal hidden literals

--- bubase.py
+++ bubase.py
@@ -1,5 +1,5 @@
 from ansa import base,constants
-from literals import Entities
+from literals import Entities,Meshes
 
 ## one property, one include.
 def dyna_part_a_include(fpath:str) -> base.Entity:
@@ -8,3 +8,11 @@ def dyna_part_a_include(fpath:str) -> base.Entity:
     num_part = len(ppts_inclu)
     assert num_part == 1, "one include now has {} part!".format(num_part)
     return ppts_inclu[0]
+
+def reveal_unrecogonized_ents(deck:int) -> list[str]:
+    ents = base.CollectEntitiesI(deck,None,'__ALL_ENTITIES__')
+    meshes_ty = [ty.value for ty in Meshes]
+    ents_ty = [ty.value for ty in Entities]
+    ents_ty.extend(meshes_ty)
+    # return meshes_ty
+    return list(set([ent.ansa_type(deck) for ent in ents if ent.ansa_type(deck) not in ents_ty]))
