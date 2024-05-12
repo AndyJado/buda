635eb36a54e93cb46cba1cdd6d5cdaff9ed6038d
mz <0922366@zju.edu.cn>
2024-05-12 11:45:23 +0800

    maybe ansa group a include

--- bubase.py
+++ bubase.py
@@ -5,6 +5,6 @@ from literals import Entities
 def dyna_part_a_include(fpath:str) -> base.Entity:
     inclu = base.InputLSDyna(fpath,header="overwrite",new_include="on",create_parameters="on")
     ppts_inclu = base.CollectEntities(constants.LSDYNA,inclu,Entities.PROPERTY,recursive=True)
-    assert len(ppts_inclu) == 1, "one include, one part!"
+    num_part = len(ppts_inclu)
+    assert num_part == 1, "one include now has {} part!".format(num_part)
     return ppts_inclu[0]
-
